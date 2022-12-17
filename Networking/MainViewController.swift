ade523cf261024eb2fd4957f4f82df38aa6525ac
Nikolai Khristoliubov <n.khristoliubov@gmail.com>
2022-12-17 15:36:38 +0400

    add MainViewController with collectionView and 2 enum (link and user actions)

---
+++ Networking/MainViewController.swift
@@ -0,0 +1,106 @@
+//
+//  MainViewController.swift
+//  Networking
+//
+//  Created by Nikolai Khristoliubov on 17.12.22.
+//
+
+import UIKit
+
+enum Link: String {
+    case imageURL = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+    case exampleOne = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+    case exampleTwo = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
+    case exampleThree = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+    case exampleFour = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
+}
+
+enum UserAction: String, CaseIterable {
+    case downloadImage = "Download Image"
+    case exampleOne = "Example One"
+    case exampleTwo = "Example Two"
+    case exampleThree = "Example Three"
+    case exampleFour = "Example Four"
+    case ourCourses = "Our Courses"
+}
+
+private let reuseIdentifier = "Cell"
+
+class MainViewController: UICollectionViewController {
+    
+    private let userActions = UserAction.allCases
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+       // self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        userActions.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! UserActionCell
+        let userAction = userActions[indexPath.item]
+        cell.userActionLabel.text = userAction.rawValue
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: (view.window?.windowScene?.screen.bounds.width ?? 200) - 40, height: 100)
+    }
+}
---
+++ Networking/UserActionCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserActionCell.swift
+//  Networking
+//
+//  Created by Nikolai Khristoliubov on 17.12.22.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    @IBOutlet var userActionLabel: UILabel!
+    
+}
