823b808aeda55210d6b7f7452ae8f16f1212d87c
Nikolai Khristoliubov <n.khristoliubov@gmail.com>
2022-12-24 15:55:17 +0400

    added checking image addresses for the current cell (Our Courses).

--- Networking/ViewControllers/CourseCell.swift
+++ Networking/ViewControllers/CourseCell.swift
@@ -15,18 +15,29 @@ class CourseCell: UITableViewCell {
     @IBOutlet var numberOfLessonsLabel: UILabel!
     @IBOutlet var numberOfTestsLabel: UILabel!
     
+    private var imageURL: String? {
+        didSet {
+            fetchCourseImage(from: imageURL)
+        }
+    }
     
     func configureCell(course: Course) {
         nameOfCourseLabel.text = course.name
         numberOfLessonsLabel.text = "Number of lessons: \(course.numberOfLessons ?? 0)"
         numberOfTestsLabel.text = "Number of tests: \(course.numberOfTests ?? 0)"
         
-        NetworkManager.shared.fetchImage(url: course.imageUrl ?? "") { result in
+        imageURL = course.imageUrl
+    }
+    
+    func fetchCourseImage(from url: String?) {
+        NetworkManager.shared.fetchImage(url: imageURL ?? "") { result in
             switch result {
             case .success(let imageData):
                 let image = UIImage(data: imageData)
                 DispatchQueue.main.async {
-                    self.courseImageView.image = image
+                    if url == self.imageURL {
+                        self.courseImageView.image = image
+                    }
                 }
             case .failure(let error):
                 print(error.localizedDescription)
